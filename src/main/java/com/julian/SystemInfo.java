51b10c23e853cfa57652a0aa6630696321a3db54
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2024-08-18 19:24:43 -0600

    todo

---
+++ src/main/java/com/julian/SystemInfo.java
@@ -0,0 +1,227 @@
+package com.julian;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.List;
+import java.util.Optional;
+
+public class  SystemInfo {
+
+    private File currentFile;
+
+    @FXML
+    private TextArea textInputArea;
+
+    @FXML
+    private TextArea textOutputArea;
+
+    @FXML
+    protected void onClickButtonFile(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+
+        // Agregar filtro de extensión para archivos .ac
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("AC files (*.ac)", "*.ac");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Obtener la ruta del directorio base del proyecto
+        String basePath = System.getProperty("user.dir");
+        // Construir la ruta relativa al directorio deseado dentro del proyecto
+        String dirPath = basePath + "/data";
+
+        File dir = new File(dirPath);
+        if (dir.exists()) {
+            fileChooser.setInitialDirectory(dir); // Establecer la carpeta inicial
+        } else {
+            showAlert(Alert.AlertType.WARNING, "Folder Not Found", "The specified folder does not exist.");
+            return;
+        }
+
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            currentFile = file;
+            System.out.println("Archivo seleccionado: " + file.getAbsolutePath());
+            loadFileContent(currentFile);
+        }
+    };
+
+    @FXML
+    protected void onClickButtonCreateNewFile(){
+        if (!textInputArea.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirm Save");
+            alert.setHeaderText("Unsaved changes");
+            alert.setContentText("You have unsaved changes. Do you want to save them before creating a new file?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                if (currentFile != null) {
+                    onClickButtonSaveFile();
+                } else {
+                    Stage stage = (Stage) textInputArea.getScene().getWindow();
+                    onClickButtonsaveAsFile();
+                }
+            }
+        }
+
+        // Mostrar un cuadro de diálogo para ingresar el nombre del archivo
+        TextInputDialog dialog = new TextInputDialog("newFile");
+        dialog.setTitle("New File");
+        dialog.setHeaderText("Create a New File");
+        dialog.setContentText("Please enter file name:");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()) {
+            String fileName = result.get();
+            // Asegurarse de que el nombre del archivo tenga la extensión .ac
+            if (!fileName.endsWith(".ac")) {
+                fileName += ".ac";
+            }
+            // Obtener la ruta del directorio base del proyecto
+            String basePath = System.getProperty("user.dir");
+            // Construir la ruta relativa al directorio deseado dentro del proyecto
+            String dirPath = basePath + "/data";
+            String filePath = dirPath + "/" + fileName;
+
+            File dir = new File(dirPath);
+            if (!dir.exists()) {
+                dir.mkdirs(); // Crear directorio si no existe
+            }
+
+            currentFile = new File(filePath);
+            try {
+                if (currentFile.createNewFile()) {
+                    System.out.println("Archivo creado: " + currentFile.getName());
+                    showAlert(Alert.AlertType.INFORMATION, "File Created", "New file created: " + currentFile.getName());
+                } else {
+                    System.out.println("El archivo ya existe.");
+                    showAlert(Alert.AlertType.WARNING, "File Exists", "File already exists.");
+                }
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error.");
+                showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while creating the file.");
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @FXML
+    protected void onClickButtonSaveFile() {
+        if (currentFile != null) {
+            try (FileWriter writer = new FileWriter(currentFile)) {
+                String content = textInputArea.getText(); // Retrieve content from textInputArea
+                writer.write(content); // Write content to the file
+                System.out.println("File saved: " + currentFile.getName());
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        } else {
+            showAlert(Alert.AlertType.WARNING, "No file to save", "No file to save, Create file or Load file before to save.");
+            System.out.println("No file to save.");
+        }
+    };
+
+    @FXML
+    protected void onClickButtonsaveAsFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+
+        // Agregar filtro de extensión para archivos .ac
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("AC files (*.ac)", "*.ac");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Obtener la ruta del directorio base del proyecto
+        String basePath = System.getProperty("user.dir");
+        // Construir la ruta relativa al directorio deseado dentro del proyecto
+        String dirPath = basePath + "/data";
+
+        File dir = new File(dirPath);
+        if (dir.exists()) {
+            fileChooser.setInitialDirectory(dir); // Establecer la carpeta inicial
+        } else {
+            showAlert(Alert.AlertType.WARNING, "Folder Not Found", "The specified folder does not exist.");
+            return;
+        }
+
+        File file = fileChooser.showSaveDialog(new Stage());
+        if (file != null) {
+            try (FileWriter writer = new FileWriter(file)) {
+                String content = textInputArea.getText(); // Retrieve content from textInputArea
+                writer.write(content); // Write content to the file
+                System.out.println("File saved: " + file.getName());
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void loadFileContent(File file) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            textInputArea.setText(content.toString());
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onClickExecution(){
+        String text = textInputArea.getText();
+        if (text.isEmpty()) {
+            showAlert(Alert.AlertType.WARNING, "Empty File", "There is no content to analyze.");
+            return;
+        }
+
+        try {
+                Lexer lexer = new Lexer(new StringReader(text));
+                parser parser = new parser(lexer);
+                parser.parse();
+
+                System.out.println("Tokens:");
+                for (Token token : lexer.tokens) {
+                    System.out.println(token);
+                }
+
+                System.out.println("Tokens:");
+                for (Token token : lexer.tokens) {
+                    System.out.println(token);
+                }
+
+                if (!lexer.lexicalErrors.isEmpty()) {
+                    System.out.println("Lexical Errors:");
+                    for (String error : lexer.lexicalErrors) {
+                        System.out.println(error);
+                    }
+                }
+
+
+        } catch (Exception e) {
+            showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while analyzing the content.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/jflex/Lex.flex
@@ -0,0 +1,215 @@
+/*
+ Siemprer se debe definir el paquete
+ en el que se encuentra la clase Lexer,
+ esto es importante para que el analizador
+ sintactico pueda encontrar la clase Lexer.
+*/
+package com.julian;
+
+/*
+    Importamos las clases necesarias para el
+    funcionamiento del analizador lexico.
+*/
+
+// Cup es la clase que contiene los simbolos que
+// se usan en el analizador sintactico.
+import java_cup.runtime.*;
+
+// Estas listas son necesarias para guardar los
+// tokens y errores lexicos.
+import java.util.ArrayList;
+import java.util.List;
+
+%% //inicio de opciones.
+
+/* ----------- Seccion de opciones y declaraciones de JFlex -------------- */
+
+%public // Hace que la clase sea publica.
+
+/*
+    Cambiamos el nombre de la clase del analizador a Lexer.
+*/
+%class Lexer
+
+/*
+    Usar Unicode para las expresiones regulares y
+    el codigo generado por JFlex .
+*/
+%unicode
+
+/*
+    Activar el contador de lineas, variable yyline.
+    Activar el contador de columna, variable yycolumn.
+*/
+%line
+%column
+/*
+    The code enclosed in and is copied verbatim into the
+    constructor of the generated class. Here, member variables
+     declared in the directive can be initialised.*/
+%init{
+    yycolumn = 1;
+%init}
+
+/*
+    Activamos la compatibilidad con Java CUP para analizadores
+    sintacticos(parser).
+*/
+
+%cup
+
+/*
+    Declaraciones
+
+    El codigo entre %{ y %} sera copiado integramente en el
+    analizador generado.
+*/
+%{
+
+    StringBuffer string = new StringBuffer();
+
+    public List<Token> tokens = new ArrayList<>();
+    public List<String> lexicalErrors = new ArrayList<>();
+
+    /*
+        Generamos un java_cup.Symbol para guardar el tipo de token
+        encontrado.
+     */
+    private Symbol symbol(int type){
+        return new Symbol(type, yyline, yycolumn);
+    }
+
+    /*
+        Generamos un Symbol para el tipo de token encontrado
+        junto con su valor.
+     */
+    private Symbol symbol(int type, Object value){
+        return new Symbol(type, yyline, yycolumn, value);
+    }
+
+    private void addToken(String type, String value) {
+        tokens.add(new Token(type, value, yyline, yycolumn, value.length()));
+    }
+
+    private void addLexicalError(String message) {
+       lexicalErrors.add("Lexical error: " + message + " at line " + yyline + ", column " + yycolumn);
+     }
+
+%}
+
+%eofval{
+    return symbol(sym.EOF);
+%eofval}
+
+/*
+    Macro declaraciones
+
+    Declaramos expresiones regulares que despues usaremos en las
+    reglas lexicas.
+*/
+
+// Definimos comentarios
+LineTerminator = \r|\n|\r\n
+InputCharacter = [^\r\n]
+ws             = {LineTerminator} | [ \t\f]
+
+/* comments */
+Comment = {LineComment} | {MultiLineComment}
+
+// Comentarios de una solo linea con #
+LineComment      = "#" {InputCharacter}* {LineTerminator}?
+MultiLineComment = "<!" {CommentContent} "!>"
+CommentContent   = ([^!] | \!+ [^>])*
+
+/*
+    Letra es un caracter entre a y z o entre A y Z.
+*/
+letter = [a-zA-Z]
+/*
+    Definimos un digito como un caracter entre 0 y 9.
+*/
+digit = [0-9]
+/*
+    Definimos un identificador como una letra seguida de cero o mas
+    letras o digitos.
+*/
+id = {letter}({letter}|{digit})*
+/*
+    Definimos un numero como uno o mas digitos.
+*/
+num = {digit}+(\.{digit}+)?([eE][+-]?{digit}+)?
+
+%state STRING
+
+%% // fin de opciones.
+
+/* ------------------- Reglas Lexicas ------------------- */
+
+/*
+    Esta seccion contiene expresiones regulares y acciones.
+    Las acciones son código en Java que se ejecutara cuando se
+    encuentre una entrada valida para la expresion regular
+    correspondiente.
+*/
+
+/*
+    YYINITIAL es el estado inicial del analizador lexico al escanear.
+    Las expresiones regulares solo serán comparadas si se encuentra
+    en ese estado inicial. Es decir, cada vez que se encuentra una
+    coincidencia el scanner vuelve al estado inicial. Por lo cual se
+    ignoran estados intermedios.
+*/
+
+/* Reglas de Operaciones */
+<YYINITIAL> CONJ    { addToken("CONJ", yytext()); return symbol(sym.CONJ); }
+<YYINITIAL> OPERA   { addToken("OPERA", yytext()); return symbol(sym.OPERA); }
+<YYINITIAL> EVALUAR { addToken("EVALUAR", yytext()); return symbol(sym.EVALUAR); }
+
+/* Reglas de opereaciones Conjuntos */
+<YYINITIAL>  U      { addToken("U", yytext()); return symbol(sym.UNION); }
+<YYINITIAL>  &      { addToken("&", yytext()); return symbol(sym.INTERSECCION); }
+<YYINITIAL> \^      { addToken("^", yytext()); return symbol(sym.COMPLEMENTO); }
+<YYINITIAL>  -      { addToken("-", yytext()); return symbol(sym.DIFERENCIA); }
+
+<YYINITIAL>{
+    /* Reglas de Caracteres */
+    {id}        { addToken("ID", yytext()); return symbol(sym.ID, yytext()); }
+    {num}       { addToken("NUM", yytext()); return symbol(sym.NUM, Double.parseDouble(yytext())); }
+
+    /* Operadores */
+    "{"         { addToken("{", yytext()); return symbol(sym.LBRACE); }
+    "}"         { addToken("}", yytext()); return symbol(sym.RBRACE); }
+    "("         { addToken("(", yytext()); return symbol(sym.LPAREN); }
+    ")"         { addToken(")", yytext()); return symbol(sym.RPAREN); }
+    "->"        { addToken("->", yytext()); return symbol(sym.ARROW); }
+    ";"         { addToken(";", yytext()); return symbol(sym.SEMICOLON); }
+    ":"         { addToken(":", yytext()); return symbol(sym.COLON); }
+    "~"         { addToken("~", yytext()); return symbol(sym.VIRGULILLA); }
+    ","         { addToken(",", yytext()); return symbol(sym.COMMA); }
+
+    /* Reglas de Comentarios */
+    {Comment}    { /* ignora comentarios de una linea */ }
+
+    /* Reglas de espcios */
+    {ws}        { /* ignore whitespace */ }
+}
+
+/* Reglas para cadenas de caracteres */
+<STRING> {
+    \"                             { yybegin(YYINITIAL);
+                                   return symbol(sym.STRING_LITERAL,
+                                   string.toString()); }
+    [^\n\r\"\\]+                   { string.append( yytext() ); }
+    \\t                            { string.append('\t'); }
+    \\n                            { string.append('\n'); }
+
+    \\r                            { string.append('\r'); }
+    \\\"                           { string.append('\"'); }
+    \\                             { string.append('\\'); }
+}
+
+/*
+    Si el token contenido en la entrada no coincide con ninguna regla
+    entonces se marca un token ilegal
+*/
+[^] { addLexicalError("Illegal character: " + yytext()); }
