541bf7334afef9a4b1ac830747a718c5eeeeac34
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2024-12-26 17:32:41 -0600

    Validando Vectores

--- data/Vectores.cs
+++ data/Vectores.cs
@@ -1,7 +1,13 @@
 
 void mF( a:int = 10, b:int = 20){
-
+    let valor3 : string = vector1[0];
     const valor4:int = vector2[0][0];
+    console.log(valor3);
+    console.log(valor4);
+    vector1[0] = "OLC1";
+    vector2[0][0] = 0;
+    console.log(vector1[0]);
+    console.log(vector2[0][0]);
 
 }
 
